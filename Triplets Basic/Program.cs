40848e8e7a943473cd88f5a0d60b93cee988c816
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-06 22:24:51 +0000

    test

--- Triplets Basic/Program.cs
+++ Triplets Basic/Program.cs
@@ -13,6 +13,7 @@ namespace Triplets_Basic
             //Note: You could also allow the user to input the array list in the console window and then compute any triplets as per below. 
 
             //Array object created. 
+
             int[] integerArray = { 5, 5, 5, 5, 7, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 8, 9, 9, 9, };
 
             //LINQ used to access Array elements, group them by any that appear in 3's and select & return the first element in the sequence. 
@@ -21,8 +22,14 @@ namespace Triplets_Basic
             Console.WriteLine(string.Join(", ", triplets));
             //The number of triplets are counted and output to the console window. 
             Console.WriteLine("Number of triplets in the Array = {0}", triplets.Count());
+
             Console.ReadLine();
-            
         }
+
+        
+            
+
+
+        
     }
 }
