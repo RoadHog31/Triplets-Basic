8e5a7d648f93090c59f1862ce10cc3954d83dc75
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-06 22:37:06 +0000

    Added FirstOrDefault

--- Triplets Basic/Program.cs
+++ Triplets Basic/Program.cs
@@ -17,7 +17,9 @@ namespace Triplets_Basic
             int[] integerArray = { 5, 5, 5, 5, 7, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 8, 9, 9, 9, };
 
             //LINQ used to access Array elements, group them by any that appear in 3's and select & return the first element in the sequence. 
-            var triplets = integerArray.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.First());
+            var triplets = integerArray.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.FirstOrDefault());
+
+            triplets.ToArray();
             //This is then wrote out in the console window with commas between. 
             Console.WriteLine(string.Join(", ", triplets));
             //The number of triplets are counted and output to the console window. 
