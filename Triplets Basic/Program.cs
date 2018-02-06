990389811c71665d1fb109bcaaebdf2d5a3918a9
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-06 19:40:25 +0000

    Triplet test

--- Triplets Basic/Program.cs
+++ Triplets Basic/Program.cs
@@ -10,6 +10,13 @@ namespace Triplets_Basic
     {
         static void Main(string[] args)
         {
+            int[] integerArray = { 5, 5, 5, 5, 7, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 8 , 9, 9, 9 };
+
+            //int countTriplets = integerArray.GroupBy(e => e).Where(e => e.Count() == 5).Select(e => e.First());
+
+            var triplets = integerArray.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.First());
+            Console.WriteLine(string.Join(", ", triplets));
+            Console.ReadLine();
         }
     }
 }
