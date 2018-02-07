6b8ac92ec1a11dade00cbf54e56d22bc4aa8f752
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-07 19:33:18 +0000

    Method modified

--- Triplets Basic/GetTriplet.cs
+++ Triplets Basic/GetTriplet.cs
@@ -9,14 +9,9 @@ namespace Triplets_Basic
     class GetTriplet
     {
         public int FindTriplet(int[] input)
-        {
-
-               
-
-
-
+        {       
             //LINQ used to access Array elements, group them by any that appear in 3's and select & return the first element in the sequence. 
-            var triplets = input.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.FirstOrDefault());
+            var triplets = input.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.First());
 
             int answer = triplets.Any() ? 3 : 0;
 
--- Triplets Basic/Program.cs
+++ Triplets Basic/Program.cs
@@ -10,11 +10,11 @@ namespace Triplets_Basic
     {
         static void Main(string[] args)
         {
-            //Note: You could also allow the user to input the array list in the console window and then compute any triplets as per below. 
-            int[] input = { 5, 5, 5, 5, 7, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 8, 9, 9, 9, };
+            
+            int[] input = { 5, 5, 5, 5, 7, 7, 7, 7, 9, 9, 9, 9, 10, 10, 10, 10, 8, 8, 8, 9, 9, 9, };
 
             GetTriplet getIt = new GetTriplet();
-            Console.WriteLine(getIt.FindTriplet(input));
+            Console.WriteLine("Number of triples found are: {0}", getIt.FindTriplet(input));
             Console.ReadLine();
         }
 
