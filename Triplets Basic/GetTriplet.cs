2531f415cb40434dd2fd3d06b6e4c65c685700ea
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-07 19:26:10 +0000

    Metho created to handle triple search

---
+++ Triplets Basic/GetTriplet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triplets_Basic
+{
+    class GetTriplet
+    {
+        public int FindTriplet(int[] input)
+        {
+
+               
+
+
+
+            //LINQ used to access Array elements, group them by any that appear in 3's and select & return the first element in the sequence. 
+            var triplets = input.GroupBy(x => x).Where(y => y.Count() == 3).Select(z => z.FirstOrDefault());
+
+            int answer = triplets.Any() ? 3 : 0;
+
+            return answer;
+        }
+    }
+}
